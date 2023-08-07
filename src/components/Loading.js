8d14f6ee22cd13a0174fd3090efacbb3a083c470
lusbnz <tslcvnm@gmail.com>
2023-08-07 14:10:41 +0700

    final

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div>
+        <div className="h-screen flex items-center justify-center">
             Loading...
         </div>
     )
